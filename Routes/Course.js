3176d7ecd5d5ec5ac194bd90c50926819aac489c
Andi-gech <andi.fab23@gmail.com>
2023-09-28 19:04:46 +0300

    first commit

--- Routes/Course.js
+++ Routes/Course.js
@@ -11,17 +11,7 @@ const {
 } = require("../Models/EnrolledCourse");
 const { Lessons, checklessonvalidation } = require("../Models/Lessons");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const fileExtension = file.originalname.split(".").pop();
-    cb(null, `${file.fieldname}-${uniqueSuffix}.${fileExtension}`);
-  },
-});
-const upload = multer({ storage });
+const upload = multer();
 
 // Get all courses with author's first name
 router.get("", authmiddleware, async (req, res) => {
@@ -202,7 +192,7 @@ router.post(
 
       const course = new Course({
         name: req.body.name,
-        coursepic: coursepic,
+        coursepic: req.file.buffer,
         description: req.body.description,
         Author: req.user._id,
         Price: req.body.Price,
